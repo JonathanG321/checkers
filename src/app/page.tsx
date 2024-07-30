4f1a3942553a404e69b5e8c042a434c74072de5b
JonathanG321 <JonathanG321@ymail.com>
2024-07-30 14:56:56 -0400

    make page look nicer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,22 @@ import { useEffect, useState } from 'react';
 import { socket } from '../socket';
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState('N/A');
   const [lastId, setLastId] = useState('');
-  const [room, setRoom] = useState('');
+  const [room, setRoom] = useState<undefined | string>(undefined);
   const [occupants, setOccupants] = useState<string[]>([]);
 
+  function onClick() {
+    socket.emit('updateLastId', socket.id);
+  }
+
+  function joinRoom() {
+    socket.emit('joinRoom', socket.id);
+  }
+
   useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-
-      socket.io.engine.on('upgrade', (transport) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport('N/A');
-    }
-
     function onNewLastId(id: string) {
       setLastId(id);
     }
-
     function onUpdateRoomOccupants(newOccupants: string[]) {
       setOccupants(newOccupants);
     }
@@ -40,38 +27,39 @@ export default function Home() {
       setRoom(name);
     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
     socket.on('newLastId', onNewLastId);
     socket.on('updateRoomOccupants', onUpdateRoomOccupants);
     socket.on('updateRoomName', onUpdateRoomName);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
   }, []);
 
-  function onClick() {
-    socket.emit('updateLastId', socket.id);
-  }
-
   return (
-    <div className="m-10">
-      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
-      <p>Transport: {transport}</p>
-      <p>Last Id: {lastId}</p>
-      <button onClick={onClick} disabled={lastId === socket.id} className="p-2 border-1 bg-gray-800 text-white rounded">
-        Update Last ID
-      </button>
-      <div className="flex flex-col">
-        <h1>{room}</h1>
-        {occupants.map((occupant) => (
-          <div key={occupant} className="p-1">
-            {occupant}
+    <div className="m-10 flex justify-center items-center">
+      {!room && (
+        <button onClick={joinRoom} className="p-2 border-1 bg-gray-800 text-white rounded">
+          Join Room
+        </button>
+      )}
+      {!!room && (
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold">{room}</h1>
+            <p>Last Id: {lastId}</p>
+            <div className="flex flex-col border border-white p-2">
+              <h2 className="text-xl font-bold">Occupants</h2>
+              {occupants.map((occupant) => (
+                <div key={occupant}>{occupant}</div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+          <button
+            onClick={onClick}
+            disabled={lastId === socket.id}
+            className="p-2 border text-white rounded border-white disabled:bg-gray-800 disabled:text-gray-300"
+          >
+            Update Last ID
+          </button>
+        </div>
+      )}
     </div>
   );
 }
